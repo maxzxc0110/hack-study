bb3efd04005cc89995531f45132c7aed7e4e22f4
273206894@qq.com <273206894@qq.com>
2023-03-31 16:33:04 +0800

    vuex-demo

--- 编程/vuex/vuex-demo1/src/store/index.js
+++ 编程/vuex/vuex-demo1/src/store/index.js
@@ -10,9 +10,36 @@ export default new Vuex.Store({
 		list: [],
 		count: 123,
 		inputValue:"aaa",
-		nextID:5
+		nextID:5,
+		viewKey:"all"
 	},
-	getters: {},
+	getters: {
+		undoneLength:state=>{   //定义一个undoneLength，统计未完成条数
+			return state.list.filter(x => x.done === false).length
+		},
+		doneList:state=>{    //定义一个doneList，统计已完成
+			return state.list.filter(x => x.done === false)
+		},
+		undoneList:state=>{ //定义一个undoneList，统计未完成
+			return state.list.filter(x => x.done === true)
+		},
+		
+		 //这里的实现还有另外一个方法，参考HelloWorld.ve里的getList
+		getInfoList:state=>{  //直接在getters里定义一个获取当前状态的函数供页面端调用
+			
+			if (state.viewKey === 'all'){
+				return state.list
+			}
+			
+			if (state.viewKey === 'undone'){
+				return state.list.filter(x => x.done === true)
+			}
+			
+			if(state.viewKey === 'done'){
+				return state.list.filter(x => x.done === false)
+			}
+		}
+	},
 	mutations: { //同步操作，只有mutations能修改state的值
 		initList(state, list) {
 			state.list = list
@@ -29,6 +56,25 @@ export default new Vuex.Store({
 			state.list.push(obj)
 			state.nextID++
 			state.inputValue = ""
+		},
+		removeItem(state,id){
+			const i = state.list.findIndex(x => x.id === id)  //查找对应ID的数组下标
+			
+			if(i !=-1){
+				state.list.splice(i,1)  //删除列表里对应下标的数据，表示从下标i开始，删除1位数据
+			}
+		},
+		modifyStatus(state,param){
+			const i = state.list.findIndex(x =>x.id === param.id) //根据传入id找到下标
+			if( i != -1){
+				state.list[i].done = param.status  //修改状态值
+			}
+		},
+		clearDone(state){
+			state.list = state.list.filter(x =>x.done === false)  //查找所有未完成条目，重新赋值到list
+		},
+		modifyKey(state,key){
+			state.viewKey = key
 		}
 	},
 	actions: { //异步操作
