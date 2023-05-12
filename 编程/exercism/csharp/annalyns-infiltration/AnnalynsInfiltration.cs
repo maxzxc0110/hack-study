57f21762383c071e7382591287eb619b72d1aa0f
273206894@qq.com <273206894@qq.com>
2023-05-12 18:21:47 +0800

    add

---
+++ 编程/exercism/csharp/annalyns-infiltration/AnnalynsInfiltration.cs
@@ -0,0 +1,32 @@
+using System;
+
+static class QuestLogic
+{
+    public static bool CanFastAttack(bool knightIsAwake)
+    {
+        
+        return !knightIsAwake;
+    }
+
+    public static bool CanSpy(bool knightIsAwake, bool archerIsAwake, bool prisonerIsAwake)
+    {
+
+
+
+        return knightIsAwake || archerIsAwake || prisonerIsAwake;
+    }
+
+    public static bool CanSignalPrisoner(bool archerIsAwake, bool prisonerIsAwake)
+    {
+
+
+        return !archerIsAwake && prisonerIsAwake;
+    }
+
+    public static bool CanFreePrisoner(bool knightIsAwake, bool archerIsAwake, bool prisonerIsAwake, bool petDogIsPresent)
+    {
+        
+
+        return !archerIsAwake && (!knightIsAwake && prisonerIsAwake || petDogIsPresent);
+    }
+}
---
+++ 编程/exercism/csharp/lucians-luscious-lasagna/LuciansLusciousLasagna.cs
@@ -0,0 +1,26 @@
+class Lasagna
+{
+    // TODO: define the 'ExpectedMinutesInOven()' method
+    public int ExpectedMinutesInOven()
+    {
+        return 40;
+    }
+
+    // TODO: define the 'RemainingMinutesInOven()' method
+    public int RemainingMinutesInOven(int var)
+    {
+        return this.ExpectedMinutesInOven() - var;   
+    }
+
+    // TODO: define the 'PreparationTimeInMinutes()' method
+    public int PreparationTimeInMinutes(int var)
+    {
+        return var * 2;
+    }
+
+    // TODO: define the 'ElapsedTimeInMinutes()' method
+    public int ElapsedTimeInMinutes(int v1,int v2)
+    {
+        return this.PreparationTimeInMinutes(v1) + v2;
+    }
+}
